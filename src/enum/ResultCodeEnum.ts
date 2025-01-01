7fedd65a7673ed1c4310003436f286b626962a79
sdnychen <sdnychen@gmail.com>
2025-01-02 00:43:12 +0800

    添加验证码

--- src/enum/ResultCodeEnum.ts
+++ src/enum/ResultCodeEnum.ts
@@ -1,4 +1,17 @@
-// enum ResultCodeEnum {
-//     SUCCESS = 20000,
-//    
-// }
+enum ResultCodeEnum {
+    SUCCESS = 20000, // 操作成功
+
+    FAIL = 40000, // 操作失败
+
+    AUTHENTICATION_FAIL = 40001, // 身份验证失败
+    NO_AUTH = 40002, // 暂无权限
+    LOGIN_FAIL = 40003, // 登录失败
+    LOGIN_EXPIRED = 40004, // 登录过期
+
+    VERIFY_ERROR = 40100, // 验证码错误
+
+    ERROR = 50000, // 系统内部错误
+    CERTIFICATE_FAIL = 50100, // 凭证获取失败
+}
+
+export default ResultCodeEnum;
